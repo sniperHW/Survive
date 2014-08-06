f2b2f2e5bcf347e2d7b5fcc5af736c0810cb82fc
sniperHW <huangweilook@21cn.com>
2014-08-06 17:25:20 +0800

    none

--- card/card/gameserver/script/handler.lua
+++
@@ -1,9 +0,0 @@
-local Game = require "script/game"
-local Avatar = require "script/avatar"
-local Dbmgr = require "script/dbmgr"
-
---注册各模块的消息处理函数
-function reghandler()
-	Game.RegHandler()
-	return Dbmgr.Init()	
-end
